a76365b84af2b6388bc5835be0f9a7da2c63eeff
aleafwolf <aleafwolf@outlook.com>
2022-06-05 00:01:33 -0400

    basic set up with graphql

---
+++ src/api/rarity/controllers/rarity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  rarity controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::rarity.rarity');
---
+++ src/api/rarity/routes/rarity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rarity router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::rarity.rarity');
