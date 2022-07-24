90e5bd31d587059019f9846563a25277ad3f9d07
LeafWolf <aleafwolf@outlook.com>
2022-07-23 23:08:32 -0400

    upgrader to 4.2.3

--- config/plugins.js
+++ config/plugins.js
@@ -12,17 +12,18 @@ module.exports = {
   //   },
   // },
   ckeditor: true,
-  // email: {
-  //   config: {
-  //     provider: "sendgrid",
-  //     providerOptions: {
-  //       apiKey: env("SENDGRID_API_KEY"),
-  //     },
-  //     settings: {
-  //       defaultFrom: "juliasedefdjian@strapi.io",
-  //       defaultReplyTo: "juliasedefdjian@strapi.io",
-  //       testAddress: "juliasedefdjian@strapi.io",
-  //     },
-  //   },
-  // },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        // apiKey: env("SENDGRID_API_KEY"),
+        apiKey: "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",
+      },
+      settings: {
+        defaultFrom: "nxxtoolbox@outlook.com",
+        defaultReplyTo: "nxxtoolbox@outlook.com",
+        testAddress: "nxxtoolbox@outlook.com",
+      },
+    },
+  },
 };
