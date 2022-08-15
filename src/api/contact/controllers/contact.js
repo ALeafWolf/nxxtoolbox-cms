b622334886340ddf947d03368988b68434a50454
LeafWolf <aleafwolf@outlook.com>
2022-08-14 21:01:22 -0400

    feat: - add send email function at /contact

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  *  contact controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::contact.contact');
+module.exports = createCoreController("api::contact.contact", ({ strapi }) => ({
+  async create(ctx) {
+    let req = ctx.request.body;
+    const entity = await strapi.service("api::contact.contact").create(req);
+    await strapi.plugins["email"].services.email.send({
+      to: process.env.ADMIN_EMAIL,
+      subject: "New Contact Submission from NXXToolbox",
+      text: `
+        Name: ${entity.name}
+        Email: ${entity.name || "N/A"}
+        Comment: 
+        ${entity.comment}
+        `,
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
